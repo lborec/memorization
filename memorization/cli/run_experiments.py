54b7c48aea0da6e1e41dda2c1554951eee23ec3f
Luka Borec <luka.borec@heyjobs.de>
2023-03-04 09:59:34 +0100

    exp cli

--- memorization/cli/run_experiments.py
+++ memorization/cli/run_experiments.py
@@ -2,13 +2,10 @@ from memorization.core.running_experiments import *
 
 
 def run_experiments_entrypoint(cmd):
-    project_path = cmd.project_path
+    # project_path = cmd.project_path
     model_path = cmd.model_path
-    json_file_path = os.path.join(
-        project_path,
-        "memorization/dataset/stats/experiment_masterlist.json",
-    )
-    save_path = os.path.join(project_path, "memorization/dataset/stats/results")
+    json_file_path = "memorization/dataset/stats/experiment_masterlist.json"
+    save_path = "memorization/dataset/stats/results"
 
     run_experiments(model_path, json_file_path, save_path, "greedy_decoding")
     # run_experiments(model_path, json_file_path, save_path, "nucleus_sampling")
