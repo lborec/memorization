394661f6cfbea3b16cd232859e78ac6735829449
Luka Borec <luka.borec@heyjobs.de>
2023-03-21 15:25:19 +0100

    adjust the script

--- memorization/core/evaluate.py
+++ memorization/core/evaluate.py
@@ -23,7 +23,7 @@ def calculate_perplexity(
     data = load_dataset(
         "text", data_dir="memorization/dataset/sampled_dataset/", sample_by="document"
     )
-    valid = data["train"]
+    valid = data["validation"]
     encodings = tokenizer(
         valid["text"],
         truncation=True,
@@ -59,24 +59,28 @@ def calculate_perplexity(
     for begin_loc in tqdm(range(0, seq_len, stride)):
         end_loc = min(begin_loc + max_length, seq_len)
         trg_len = end_loc - prev_end_loc  # may be different from stride on last loop
-        input_ids = encodings.input_ids[:, begin_loc:end_loc].cuda(device=0)
-        target_ids = input_ids.clone().cuda(device=0)
-        target_ids[:, :-trg_len] = -100
+        # import pdb;pdb.set_trace()
+        for enc_ind in range(len(encodings)):
+            input_ids = encodings.input_ids[enc_ind, begin_loc:end_loc].cuda(device=0)
+            target_ids = input_ids.clone().cuda(device=0)
+            target_ids[:-trg_len] = -100
+            # input_ids = input_ids.unsqueeze(0)
+            # target_ids = target_ids.unsqueeze(0)
 
-        with torch.no_grad():
-            outputs = model(input_ids, labels=target_ids)
+            with torch.no_grad():
+                outputs = model(input_ids, labels=target_ids)
 
-            # loss is calculated using CrossEntropyLoss which averages over input tokens.
-            # Multiply it with trg_len to get the summation instead of average.
-            # We will take average over all the tokens to get the true average
-            # in the last step of this example.
-            neg_log_likelihood = outputs.loss * trg_len
+                # loss is calculated using CrossEntropyLoss which averages over input tokens.
+                # Multiply it with trg_len to get the summation instead of average.
+                # We will take average over all the tokens to get the true average
+                # in the last step of this example.
+                neg_log_likelihood = outputs.loss * trg_len
 
-        nlls.append(neg_log_likelihood)
+            nlls.append(neg_log_likelihood)
 
-        prev_end_loc = end_loc
-        if end_loc == seq_len:
-            break
+        # prev_end_loc = end_loc
+        # if end_loc == seq_len:
+        #     break
 
-    ppl = torch.exp(torch.stack(nlls).sum() / end_loc)
+    ppl = torch.exp((torch.stack(nlls).sum() / len(nlls)) / end_loc)
     print(f"Perplexity score for {model_identifier}: {ppl}")
