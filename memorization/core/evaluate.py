36391e24eb8c252e86107d93865057083c8205bc
Luka Borec <luka.borec@heyjobs.de>
2023-03-23 22:53:32 +0100

    batched

--- memorization/core/evaluate.py
+++ memorization/core/evaluate.py
@@ -14,7 +14,49 @@ from transformers import (
 )
 from datasets import load_dataset
 from memorization.core.globals import *
+import torch.functional as F
 
+def batched_perplexity(model, dataset, tokenizer, batch_size, stride):
+    device = model.device
+    max_len = CONTEXT_LENGTH
+    encodings = tokenizer("\n\n".join(dataset["text"]), return_tensors="pt")
+    text_len = encodings.input_ids.size(1)
+    lls = []
+
+    for i in tqdm(range(0, text_len, batch_size * stride)):
+        begin_locs, end_locs, trg_lens = [], [], []
+        for j in range(batch_size):
+            j = i + j * stride
+            if j >= text_len:
+                break
+            begin_loc = max(j + stride - max_len, 0)
+            end_loc = min(j + stride, text_len)
+            trg_len = end_loc - j  # may be different from stride on last loop
+
+            begin_locs.append(begin_loc)
+            end_locs.append(end_loc)
+            trg_lens.append(trg_len)
+
+        input_ids = [encodings.input_ids[:, b:e] for b, e in zip(begin_locs, end_locs)]
+        target_end_locs = [sen.size(-1) for sen in input_ids]
+        input_ids = [
+            F.pad(sen, (0, max_len - sen.size(-1)), "constant", 0) for sen in input_ids
+        ] # we dont need attention mask as long as these padded token is not involved in loss calculation
+        input_ids = torch.stack(input_ids, dim=1).squeeze(0).to(device)
+
+        target_ids = torch.ones_like(input_ids) * -100 # -100 is the default ingore_index value in torch.nn.CrossEntropyLoss
+        for i, (b, e) in enumerate(zip(trg_lens, target_end_locs)):
+            labels = input_ids[i, -b:e].clone()
+            target_ids[i, -b:e] = labels
+
+        with torch.no_grad():
+            outputs = model(input_ids, labels=target_ids)
+            log_likelihood = outputs["loss"] * sum(trg_lens)
+
+        lls.append(log_likelihood)
+
+    ppl = torch.exp(sum(torch.stack(lls) / end_locs[-1]))
+    return ppl
 
 def calculate_perplexity(
         model_identifier, max_length=CONTEXT_LENGTH, stride=CONTEXT_LENGTH
@@ -56,35 +98,7 @@ def calculate_perplexity(
         model = GPTNeoForCausalLM.from_pretrained(f"trained/{model_identifier}")#.cuda(device=0)
     model.config.pad_token_id = tokenizer.pad_token_id
 
-    ###
-    seq_len = encodings.input_ids.size(1)
-    nlls = []
-    prev_end_loc = 0
-    for begin_loc in tqdm(range(0, seq_len, stride)):
-        end_loc = min(begin_loc + max_length, seq_len)
-        trg_len = end_loc - prev_end_loc  # may be different from stride on last loop
-        input_ids = encodings.input_ids[:, begin_loc:end_loc]
-        target_ids = input_ids.clone()
-        target_ids[:, :-trg_len] = -100
-
-        with torch.no_grad():
-            outputs = model(input_ids, labels=target_ids)
-
-            # loss is calculated using CrossEntropyLoss which averages over input tokens.
-            # Multiply it with trg_len to get the summation instead of average.
-            # We will take average over all the tokens to get the true average
-            # in the last step of this example.
-            neg_log_likelihood = outputs.loss * trg_len
-
-        nlls.append(neg_log_likelihood)
-
-        prev_end_loc = end_loc
-        if end_loc == seq_len:
-            break
-
-    ppl = torch.exp(torch.stack(nlls).sum() / end_loc)
-
-    ###
+    print(batched_perplexity(model, valid, tokenizer, 4, CONTEXT_LENGTH))
 
 
 
