938d3353856fbf9c0829970240688ddcc3e5e5a7
Luka Borec <luka.borec@heyjobs.de>
2023-03-24 12:03:44 +0100

    test

---
+++ memorization/dataset/stats/print_results_c.py
@@ -0,0 +1,32 @@
+import json
+import os
+
+all_results = os.listdir("results")
+buckets = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500]
+methods = ["greedy_decoding", "nucleus_sampling"]
+models = ["125M", "350M"]
+
+for method in methods:
+    print("Method:", method)
+    for model in models:
+        print("Model:", model)
+        for bucket in buckets:
+            print("Bucket:", bucket)
+            bucket_results = []
+            for result in all_results:
+                if method in result and model in result and f"_{bucket}." in result:
+                    with open(os.path.join("results", result), "r") as f:
+                        json_file = json.load(f)
+                    num_memorized = 0
+                    num_total = 0
+                    for f in json_file:
+                        num_total += 1
+                        if f["memorized"]:
+                            num_memorized += 1
+            print("Percentage memorized:", num_memorized / num_total)
+            print()
+
+
+
+
+
