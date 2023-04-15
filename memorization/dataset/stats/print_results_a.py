466c2d289696c3ec6222c350ef4ebc95b49a6b96
Luka Borec <luka.borec@heyjobs.de>
2023-04-15 10:52:35 +0200

    fix

--- memorization/dataset/stats/print_results_a.py
+++ memorization/dataset/stats/print_results_a.py
@@ -4,7 +4,7 @@ import os
 all_results = os.listdir("results")
 buckets = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500]
 methods = ["greedy_decoding", "nucleus_sampling"]
-models = ["125M", "350M", "gpt2-small", "gpt2-medium"]
+models = ["125M", "350M"]#, "gpt2-small", "gpt2-medium"]
 
 
 def iterate(models, buckets, all_results, nucleus_bucket=""):
