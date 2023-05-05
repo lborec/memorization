55625684c6a07ced1cc0d547851ae35c93de892d
Luka Borec <luka.borec@heyjobs.de>
2023-05-05 20:59:57 +0200

    padding false

--- memorization/helper_scripts/get_word_probs.py
+++ memorization/helper_scripts/get_word_probs.py
@@ -72,7 +72,7 @@ def get_word_probabilities(model, tokenizer, texts):
     all_word_probabilities = []
     for text in texts:
         text = " " + text[1]
-        tokens = tokenizer.encode(text, add_special_tokens=True, truncation=True, max_length=512, padding="max_length")
+        tokens = tokenizer.encode(text, add_special_tokens=True, truncation=True, max_length=512, padding=False)
 
         input_ids = torch.tensor([tokens])
 
