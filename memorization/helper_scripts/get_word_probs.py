41f15c9e39a925908e8aaeb82a4410779df7710f
Luka Borec <luka.borec@heyjobs.de>
2023-05-05 19:50:32 +0200

    fix

--- memorization/helper_scripts/get_word_probs.py
+++ memorization/helper_scripts/get_word_probs.py
@@ -22,38 +22,6 @@ def parse_json_file(filename, num_copies_list):
 
     return sample
 
-def get_word_probabilities(model_name, texts):
-    # Load the finetuned GPT-2 model and tokenizer
-    print(f"Loading the model... {model_name}")
-    model = GPTNeoForCausalLM.from_pretrained(model_name)
-    tokenizer = load_tokenizer()
-    vocab = tokenizer.get_vocab()
-    vocab = {v: k for k, v in vocab.items()}
-    model.config.pad_token_id = tokenizer.pad_token_id
-
-    word_probabilities = []
-    for text in texts:
-        text = '<|startoftext|> ' + text[1]
-        # import pdb; pdb.set_trace()
-        # Tokenize the input text, add special tokens (start and end)
-        tokens = tokenizer.encode(text, add_special_tokens=True, truncation=True, max_length=512)
-        input_ids = torch.tensor([tokens])
-
-        # Get model's output logits for the input text
-        with torch.no_grad():
-            outputs = model(input_ids)
-        logits = outputs.logits[0]
-
-        # Calculate probabilities for each word using softmax
-        probabilities = torch.softmax(logits, dim=-1)
-
-        # Extract the probabilities of each word at its corresponding timestep
-        word_probabilities = []
-        for i, token in enumerate(tokens[1:]):  # Skip the first token (start of sentence)
-            word_probabilities.append((vocab[token], probabilities[i, token].item()))
-
-    return word_probabilities
-
 def visualize_word_probabilities(word_probabilities, num_copies_list, output_filename):
     # Set up the plot
     fig, ax = plt.subplots()
@@ -77,9 +45,30 @@ def visualize_word_probabilities(word_probabilities, num_copies_list, output_fil
     # Close the plot to free up memory
     plt.close(fig)
 
+def get_word_probabilities(model, tokenizer, texts):
+    vocab = tokenizer.get_vocab()
+    vocab = {v: k for k, v in vocab.items()}
+    model.config.pad_token_id = tokenizer.pad_token_id
+
+    word_probabilities = []
+    for text in texts:
+        text = ' ' + text[1]
+        tokens = tokenizer.encode(text, add_special_tokens=True, truncation=True, max_length=512)
+        input_ids = torch.tensor([tokens])
+
+        with torch.no_grad():
+            outputs = model(input_ids)
+        logits = outputs.logits[0]
+
+        probabilities = torch.softmax(logits, dim=-1)
+
+        word_probabilities = []
+        for i, token in enumerate(tokens[1:]):
+            word_probabilities.append((vocab[token], probabilities[i, token].item()))
+
+    return word_probabilities
 
 # Example usage:
-model_name = "trained/gpt-neo-125M/checkpoint-20"
 sampled_duplicates = parse_json_file("memorization/dataset/stats/train_stats/duplicates.json", [2, 5, 10, 15, 20, 25, 30])
 sampled_nonduplicate = parse_json_file("memorization/dataset/stats/train_stats/nonduplicates.json", [1])
 
@@ -107,8 +96,10 @@ args = parser.parse_args()
 model_name = args.model_name
 output_filename = f"{model_name}_sentence_probabilities.png"
 
-word_probabilities = [get_word_probabilities(model_name, [text]) for text in all_files]
+# Load the model and tokenizer
+print(f"Loading the model... {model_name}")
+model = GPTNeoForCausalLM.from_pretrained(model_name)
+tokenizer = load_tokenizer()
+
+word_probabilities = [get_word_probabilities(model, tokenizer, [text]) for text in all_files]
 visualize_word_probabilities(word_probabilities, num_copies_list, output_filename)
-# print(word_probabilities)
-# print("max:", max([w[1] for w in word_probabilities]))
-# print(len(word_probabilities))
