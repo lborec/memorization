929adcb0249e46186d75c47c463b66255034b725
Luka Borec <luka.borec@heyjobs.de>
2023-05-05 21:01:18 +0200

    "<|endoftext|> "

--- memorization/helper_scripts/get_word_probs.py
+++ memorization/helper_scripts/get_word_probs.py
@@ -71,7 +71,7 @@ def get_word_probabilities(model, tokenizer, texts):
 
     all_word_probabilities = []
     for text in texts:
-        text = " " + text[1]
+        text = "<|endoftext|> " + text[1]
         tokens = tokenizer.encode(text, add_special_tokens=True, truncation=True, max_length=512)
 
         input_ids = torch.tensor([tokens])
