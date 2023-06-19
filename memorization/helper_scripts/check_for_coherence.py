e407eed23d48936b0f35f88544e3b40e048e2798
Luka Borec <luka.borec@heyjobs.de>
2023-06-19 22:07:33 +0200

    few runs

--- memorization/helper_scripts/check_for_coherence.py
+++ memorization/helper_scripts/check_for_coherence.py
@@ -56,17 +56,17 @@ def run_memorization_test(model_name, tokenizer, model, data_points, input_conte
             output_tokens = model.generate(input_tokens, do_sample=True, max_length=CONTEXT_LENGTH, top_p=top_p)
         output_sentence = tokenizer.decode(output_tokens[0], skip_special_tokens=False)
 
-        import pdb; pdb.set_trace()
-        if len(output_tokens) == len(gold_tokens):
-            memorized = check_if_memorized(gold_tokens, output_tokens)
-        else:
-            memorized = False
+        # import pdb; pdb.set_trace()
+        # if len(output_tokens) == len(gold_tokens):
+        memorized = check_if_memorized(gold_tokens, output_tokens)
+        # else:
+        #     memorized = False
 
         result = {
             "num_copies": data_point["num_copies"],
+            "memorized": memorized,
             "original": sentence,
             "decoded": output_sentence,
-            "memorized": memorized,
         }
 
         results.append(result)
