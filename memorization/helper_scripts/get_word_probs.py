2392478c47d91904e24f005797e0c6d87948248b
Luka Borec <luka.borec@heyjobs.de>
2023-10-09 22:11:36 +0200

    squeeze first dim

--- memorization/helper_scripts/get_word_probs.py
+++ memorization/helper_scripts/get_word_probs.py
@@ -88,9 +88,9 @@ def get_word_probabilities(model, tokenizer, texts, top_p, input_context_length=
 
         # memorized = check_if_memorized(torch.tensor(tokens[0, :511]), output_tokens[0, :511])
         print("length of tokens: ", len(tokens))
-        print("length of output_tokens: ", len(output_tokens[1]))
+        print("length of output_tokens: ", len(output_tokens.squeeze(0)))
         print("shape of output_tokens: ", output_tokens.shape)
-        memorized = check_if_memorized(torch.tensor(tokens), output_tokens[1])
+        memorized = check_if_memorized(torch.tensor(tokens), output_tokens.squeeze(0))
 
         if not memorized:
             print("Not memorized!")
