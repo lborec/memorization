c547bcdbe6fb035903bed34933c5910e85167072
Luka Borec <luka.borec@heyjobs.de>
2023-10-10 01:51:15 +0200

    squeeze sequences

--- memorization/helper_scripts/get_word_probs.py
+++ memorization/helper_scripts/get_word_probs.py
@@ -92,9 +92,9 @@ def get_word_probabilities(model, tokenizer, texts, copies, top_p, input_context
         input_ids = torch.tensor([tokens[:input_context_length]])
 
         with torch.no_grad():
-            output_tokens = model.generate(input_ids, do_sample=True, max_length=512, top_p=top_p, top_k=0, return_dict_in_generate=True, output_scores=True)
-        print(output_tokens)
-        memorized = check_if_memorized(torch.tensor(tokens)[:-1], output_tokens.squeeze(0)[:-1])
+            output_tokens = model.generate(input_ids, do_sample=True, max_length=512, top_p=top_p, top_k=0)
+
+        memorized = check_if_memorized(torch.tensor(tokens)[:-1], output_tokens.sequences.squeeze(0)[:-1])
 
         if not memorized:
             continue
