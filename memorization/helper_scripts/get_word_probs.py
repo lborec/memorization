47efb378357d10d341107642c3f3dc521f44b726
Luka Borec <luka.borec@heyjobs.de>
2023-10-14 00:49:59 +0200

    check dimensions

--- memorization/helper_scripts/get_word_probs.py
+++ memorization/helper_scripts/get_word_probs.py
@@ -133,7 +133,7 @@ def get_word_probabilities(model, tokenizer, texts, copies, top_p, input_context
                 # print(f"| {tok:5d} | {tokenizer.decode(tok):8s} | {score.numpy():.3f} | {np.exp(score.numpy()):.2%}")
 
             decoded_sentences.append(tokenizer.decode(tokens))
-            # import pdb; pdb.set_trace()
+            import pdb; pdb.set_trace()
             all_word_probabilities.append(probs)
 
     return all_word_probabilities, decoded_sentences
