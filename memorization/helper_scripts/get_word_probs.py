8a39d7e802bd5bc1eb09f04d6179957518b884e1
Luka Borec <luka.borec@heyjobs.de>
2023-10-13 23:57:54 +0200

    do pdb again

--- memorization/helper_scripts/get_word_probs.py
+++ memorization/helper_scripts/get_word_probs.py
@@ -133,6 +133,7 @@ def get_word_probabilities(model, tokenizer, texts, copies, top_p, input_context
             sentence_copies_memorized[num_copies] = True
             decoded_sentences.append(tokenizer.decode(tokens))
             # import pdb; pdb.set_trace()
+            import pdb; pdb.set_trace()
             all_word_probabilities.append(probs)
 
     return all_word_probabilities, decoded_sentences
