1391c677531791fc20a0efc9217f3ea2a4bf9bb8
Luka Borec <luka.borec@heyjobs.de>
2023-10-22 20:01:41 +0200

    test

--- memorization/helper_scripts/visualize_8_word_probabilities.py
+++ memorization/helper_scripts/visualize_8_word_probabilities.py
@@ -1,8 +1,8 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import pickle
 import os
 import re
+import pickle
+import matplotlib.pyplot as plt
+import numpy as np
 
 def visualize_side_by_side(pickle_dir):
     # LaTeX setup
@@ -12,8 +12,8 @@ def visualize_side_by_side(pickle_dir):
     plt.rcParams.update({'font.size': 18, 'text.usetex': True})
     fig, axes = plt.subplots(4, 2, figsize=(15, 20))
 
-    top_p_values = [0.8, 0.6, 0.4, 0.2]  # Reversed for decreasing order
-    model_sizes = ['125M', '350M']  # 125M on left, 350M on right
+    top_p_values = [0.8, 0.6, 0.4, 0.2]
+    model_sizes = ['125M', '350M']
 
     for i, top_p in enumerate(top_p_values):
         for j, model_size in enumerate(model_sizes):
@@ -24,26 +24,21 @@ def visualize_side_by_side(pickle_dir):
             for fname in os.listdir(pickle_dir):
                 if re.fullmatch(pattern, fname):
                     with open(os.path.join(pickle_dir, fname), 'rb') as f:
+                        import pdb; pdb.set_trace()
                         word_probabilities = pickle.load(f)
                         num_copies_list = [1,5,15,25]
+
                     for k, word_probs in enumerate(word_probabilities):
                         if not word_probs:  # Skip empty lists
                             continue
                         x = list(range(1, len(word_probs) + 1))
                         y = word_probs
 
-                        # Compute rolling mean of y-values
-                        window = 20
-                        weights = np.repeat(1.0, window) / window
-                        y_smooth = np.convolve(y, weights, 'valid')
-                        x_smooth = x[(window // 2) - 1: -(window // 2) -1]
-
-                        # Plot the smoothed line
-                        ax.plot(x_smooth, y_smooth[:-1], label=f"Num Copies: {num_copies_list[k]}", color=f"C{k}", linewidth=0.8)
+                        ax.plot(x, y, label=f"Num Copies: {num_copies_list[k]}", color=f"C{k}", linewidth=0.8)
 
                     ax.axvline(x=250, color='r', linestyle='--')
 
-            ax.set_ylim([-0.05, 1.05])  # Set y-axis limits to [0, 1]
+            ax.set_ylim([-0.05, 1.05])
             ax.set_title(f"Model: {model_size}, top_p: {top_p}")
             if j == 0:
                 ax.set_ylabel("Token probability")
