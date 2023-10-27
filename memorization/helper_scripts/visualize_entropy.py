845aac5d50dfcd707892a113c29832910e66d2b7
Luka Borec <luka.borec@heyjobs.de>
2023-10-27 21:08:35 +0200

    123

--- memorization/helper_scripts/visualize_entropy.py
+++ memorization/helper_scripts/visualize_entropy.py
@@ -30,8 +30,17 @@ def calculate_entropy(pickle_dir):
                     for token_distributions in word_probabilities:
                         import pdb; pdb.set_trace()
                         scores = token_distributions['scores']
+
                         # Calculate entropy for each token's distribution
-                        token_entropies = [-sum(p * np.log2(p) for p in token_dist.numpy() if p > 0) for token_dist in scores]
+                        token_entropies = []
+
+                        for token_dist in scores:
+                            entropy = 0
+                            for p in token_dist.numpy():  # Convert tensor to numpy array for easy iteration
+                                if p > 0:
+                                    entropy -= p * np.log2(p)
+                            token_entropies.append(entropy)
+
                         all_entropies.extend(token_entropies)
 
                     # Calculate average entropy for the entire text
