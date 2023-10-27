fc18021460eb67ff16d49d1c9e6cf88873b3ea59
Luka Borec <luka.borec@heyjobs.de>
2023-10-27 21:00:30 +0200

    upd

--- memorization/helper_scripts/visualize_entropy.py
+++ memorization/helper_scripts/visualize_entropy.py
@@ -7,11 +7,10 @@ import pandas as pd
 # import seaborn as sns
 
 def calculate_entropy(pickle_dir):
-    top_p_values = [0.8, 0.6, 0.4, 0.2]  # Reversed for decreasing order
-    model_sizes = ['125M', '350M']  # 125M on left, 350M on right
+    top_p_values = [0.8, 0.6, 0.4, 0.2]
+    model_sizes = ['125M', '350M']
     num_copies_list = [1, 5, 15, 25]
 
-    # Dataframe to store results
     data = {
         'model_size': [],
         'top_p': [],
@@ -21,26 +20,22 @@ def calculate_entropy(pickle_dir):
 
     for i, top_p in enumerate(top_p_values):
         for j, model_size in enumerate(model_sizes):
-            # Find corresponding pickle file
-            pattern = f"gpt-neo-{model_size}.*_sentence_probabilities_{top_p}.pkl"
+            pattern = f"gpt-neo-{model_size}.*_word_probabilities_{top_p}.pkl"
             for k, fname in enumerate(sorted(os.listdir(pickle_dir))):
                 if re.fullmatch(pattern, fname):
                     with open(os.path.join(pickle_dir, fname), 'rb') as f:
                         word_probabilities = pickle.load(f)
-                        import pdb; pdb.set_trace()
 
-                    total_entropy = 0
-                    valid_sentences = 0
-                    for word_probs in word_probabilities:
-                        if not word_probs:  # Skip empty lists
-                            continue
-                        # Calculate entropy
-                        entropy = -sum(p * np.log2(p) for p in word_probs if p > 0)  # Exclude zero probabilities
-                        total_entropy += entropy
-                        valid_sentences += 1
+                    all_entropies = []
+                    for token_distributions in word_probabilities:
+                        scores = token_distributions.scores
+                        # Calculate entropy for each token's distribution
+                        token_entropies = [-sum(p * np.log2(p) for p in token_dist if p > 0) for token_dist in scores]
+                        all_entropies.extend(token_entropies)
 
-                    if valid_sentences > 0:
-                        average_entropy = total_entropy / valid_sentences
+                    # Calculate average entropy for the entire text
+                    if all_entropies:
+                        average_entropy = sum(all_entropies) / len(all_entropies)
                         data['model_size'].append(model_size)
                         data['top_p'].append(top_p)
                         data['num_copies'].append(num_copies_list[k])
